227cbbcd56787eff762cb2e675d75aa8a32a94c6
aryangurau <aryangurau143@gmail.com>
2024-07-31 10:36:21 +0545

    updated index

--- index.js
+++ index.js
@@ -7,11 +7,30 @@ http.createServer();
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
+    //logic to check RAM in GB?
+    const byteToGb = 1073741824;
+    const myRam = os.totalmem() / byteToGb;
+    const availableRam = os.freemem() / byteToGb;
+
     res.end(
       JSON.stringify({
-        data: "Hello World!",
+        data: {
+          myRam: `you have ${myRam} Gb RAM`,
+          availableRam: `you have ${availableRam}GB free out of ${myRam} Gb RAM `,
+        },
       })
     );
   })
   .listen(8000); //port 3000-9999
 console.log("Application is Running");
+
+//importing own modules from text.js
+
+const text = require("./text");
+console.log(text.lowerCase("aryAn GuRau"));
+
+const Num = require("./Number");
+console.log(Num.properNumber(32323343));
+
+const sum = require("./Number");
+console.log(sum.sumNum(3, 3));

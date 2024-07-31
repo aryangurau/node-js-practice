c21cb7ba49fda1bf08a1552df2526ba4ccb5bb58
aryangurau <aryangurau143@gmail.com>
2024-07-31 10:36:41 +0545

    created filesystem server

---
+++ fs.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+//write a js function to read the content of the file using fs module
+// result: hello this is file from hello.txt
+
+// const readFile = (path) => {
+//   return fs.readFile("path", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//     return;
+//   });
+// };
+// console.log(readFile("hello.txt"));
+

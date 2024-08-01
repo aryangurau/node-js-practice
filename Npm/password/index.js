6744ca15534ae581cb0a861cce8c5bf26b1cf6d7
aryangurau <aryangurau143@gmail.com>
2024-08-01 19:11:10 +0545

    updated index and practiced password hashing

--- Npm/password/index.js
+++ Npm/password/index.js
@@ -8,3 +8,17 @@ const hash = genHash("Aryangurau123");
 console.log({ hash });
 const checkPw = compareHash("Aryangurau123", hash);
 console.log({ checkPw });
+
+//practiced password hashing
+/*
+const bcrypt = require("bcryptjs");
+
+const hashGen = (string) => bcrypt.hashSync(String(string), 10);
+const compare = (string, hashedPw) =>
+  bcrypt.compareSync(String(string), hashedPw);
+
+const hash = hashGen("aryangurau122");
+console.log({ hash });
+const checkPw = compare("aryangurau122", hash);
+console.log({ checkPw });
+*/

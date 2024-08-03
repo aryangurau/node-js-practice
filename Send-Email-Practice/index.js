9e564b185312ad6f4b0776cb33983f8217838a9a
aryangurau <aryangurau143@gmail.com>
2024-08-03 09:39:54 +0545

    practiced email-sending

---
+++ Send-Email-Practice/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: process.env.SMTP_SERVICE,
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+const mailer = ({ to, subject, message }) => {
+  const result = transporter.sendMail({
+    from: `"aryan gurau <${process.env.SMTP_USERNAME}>`,
+    to,
+    subject,
+    html: message,
+  });
+  return result;
+};
+
+mailer({
+  to: "gurauaryan144@gmail.com",
+  subject: "testing",
+  message: "<h1>I practiced to create email sending app</h1>",
+});
